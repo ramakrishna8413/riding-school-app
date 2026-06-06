bc2c5658b872203ddd780d3e43252a62b27c672f
Rama Krishna <rr0227898@gmail.com>
2026-06-06 11:24:19 +0530

    Deploy front-end to GitHub Pages docs folder

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/riding-school-app/",
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "../docs",
+  },
 });
