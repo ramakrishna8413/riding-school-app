cca1da7eefbbbd2d1ea5ab8e7e23a9c94db21955
Rama Krishna <rr0227898@gmail.com>
2026-06-07 13:43:36 +0530

    Update database connection configuration

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,10 +1,6 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-console.log("DB_HOST =", process.env.DB_HOST);
-console.log("DB_PORT =", process.env.DB_PORT);
-console.log("DB_USER =", process.env.DB_USER);
-console.log("DB_NAME =", process.env.DB_NAME);
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
