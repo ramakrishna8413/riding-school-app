6ffe5a589a9506f9f3f222ee49c0054423b390fa
Rama Krishna <rr0227898@gmail.com>
2026-06-23 19:48:27 +0530

    Remove login and logout

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
 import Riders from "./pages/Riders";
@@ -9,112 +9,22 @@ import Attendance from "./pages/Attendance";
 import Payments from "./pages/Payments";
 import Invoices from "./pages/Invoices";
 import Reports from "./pages/Reports";
-import Login from "./pages/Login";
 import Profile from "./pages/Profile";
-
-function ProtectedRoute({ children }) {
-  const isLoggedIn = localStorage.getItem("isLoggedIn");
-
-  return isLoggedIn ? children : <Navigate to="/login" />;
-}
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-
-        <Route path="/login" element={<Login />} />
-
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/riders"
-          element={
-            <ProtectedRoute>
-              <Riders />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/horses"
-          element={
-            <ProtectedRoute>
-              <Horses />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/trainers"
-          element={
-            <ProtectedRoute>
-              <Trainers />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/bookings"
-          element={
-            <ProtectedRoute>
-              <Booking />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/attendance"
-          element={
-            <ProtectedRoute>
-              <Attendance />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/payments"
-          element={
-            <ProtectedRoute>
-              <Payments />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/invoices"
-          element={
-            <ProtectedRoute>
-              <Invoices />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/reports"
-          element={
-            <ProtectedRoute>
-              <Reports />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/riders" element={<Riders />} />
+        <Route path="/horses" element={<Horses />} />
+        <Route path="/trainers" element={<Trainers />} />
+        <Route path="/bookings" element={<Booking />} />
+        <Route path="/attendance" element={<Attendance />} />
+        <Route path="/payments" element={<Payments />} />
+        <Route path="/invoices" element={<Invoices />} />
+        <Route path="/reports" element={<Reports />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </HashRouter>
   );
