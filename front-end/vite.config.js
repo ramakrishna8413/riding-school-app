3e4b96dcd9f31b664ed36885e1537d85c0f263f3
ramakrishna8413 <rr0227898@gmail.com>
2026-09-24 11:31:28 +0530

    Fix base path for Render

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,11 +1,23 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   base: "/riding-school-app/",
+//   plugins: [react(), tailwindcss()],
+//   build: {
+//     outDir: "dist",
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/riding-school-app/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: "dist",
-  },
+    outDir: "dist"
+  }
 });
