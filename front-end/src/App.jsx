07a381c999f456aa2aef0a8312431c9a308d7e7f
ramakrishna8413 <rr0227898@gmail.com>
2026-09-24 11:15:52 +0530

    Fix dashboard import

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,6 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/dashboard";
 import Riders from "./pages/riders";
 import Horses from "./pages/horses";
 import Trainers from "./pages/trainers";
